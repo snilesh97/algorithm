50738c3b016587ba50bfb1489043f1339df6ca0b
Nilesh <snilesh97@gmail.com>
2020-09-14 16:28:14 +0530

    [LEETCODE-198] HouseRobber

---
+++ HouseRobber.java
@@ -0,0 +1,35 @@
+// LeetCode problem Number 198 House Robber
+// https://leetcode.com/problems/house-robber
+
+class HouseRobber {
+	public int rob(int[] nums) {
+
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+
+		if (nums.length == 1) {
+			return nums[0];
+		}
+
+		if (nums.length == 2) {
+			return Math.max(nums[0], nums[1]);
+		}
+
+		int[] dp = new int[nums.length];
+
+		dp[0] = nums[0];
+		dp[1] = Math.max(nums[0], nums[1]);
+		for (int i = 2; i < nums.length; i++) {
+			dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+		}
+
+		return dp[nums.length - 1];
+	}
+
+	public static void main(String[] args) {
+		HouseRobber houseRobber = new HouseRobber();
+		int[] nums = { 17, 18, 5, 4, 6, 1 };
+		System.out.println(houseRobber.rob(nums));
+	}
+}
